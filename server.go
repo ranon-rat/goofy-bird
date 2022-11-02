a01e6fa87f6b4c2ab46dbc284e10a37b645c9581
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2022-11-01 18:20:00 -0600

    ya funciona esta mierda :+1:

--- server.go
+++ server.go
@@ -6,8 +6,6 @@ import (
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/upload-model", func(w http.ResponseWriter, r *http.Request) {
 
-	})
 	http.ListenAndServe(":8080", nil)
 }
--- src/agent.ts
+++ src/agent.ts
@@ -24,21 +24,22 @@ export class Agent {
     epsilon: number = 80
     gamma: number = 0.9
     memory: memory[] = []
-    model: Brain = new Brain([4, 4, 2], ["sigmoid", "sigmoid"])
+    model: Brain = new Brain([3, 4, 2], ["sigmoid", "sigmoid"])
     shortMemory: memory[] = []
+    score:number=0
     getState(g: Game): number[] {
         let distanceDownPlatform = (g.bird.y - g.obstacle.freeSpaceY) / g.height
         let distanceUpPlatform = ((g.obstacle.freeSpaceY + g.obstacle.freeSpaceHeight) - (g.bird.y + g.bird.height)) / g.height
         let distanceRight = (g.obstacle.x - (g.bird.x + g.bird.width)) / g.width
-        return [(distanceDownPlatform), (distanceUpPlatform), distanceRight, g.bird.velY / g.bird.jumpVel]
+    return [Math.abs((distanceDownPlatform)), Math.abs((distanceUpPlatform)), distanceRight]
     }
     remember() {
 
         this.memory.concat(this.shortMemory)
         this.memory = shuffle(this.shortMemory)
 
-        if (this.memory.length > 50000) {
-            this.memory = this.memory.slice(0, 49999)
+        if (this.memory.length > 5000) {
+            this.memory = this.memory.slice(0, 4999)
         }
     }
 
@@ -63,9 +64,14 @@ export class Agent {
         )
         return this.model.backprop(layers, loss)
     }
-
+    shorMemoryTrain(lr: number) {
+        this.shortMemory.map(i => this.backAndLoss(i)).map(i => this.model.update(lr / this.shortMemory.length, i[0], i[1]))
+    }
     longMemoryTrain(lr: number) {
         let minisample = shuffle(this.memory)
+        if(minisample.length>500){
+            minisample=minisample.slice(0,500)
+        }
 
         minisample.map((m) => this.backAndLoss(m)).map(i =>
 
@@ -85,11 +91,13 @@ export class Agent {
 
             g.bird.jump()
         }
+
         g.obstacle.move()
         g.bird.move()
         let done = g.obstacle.collide(g.bird)
-
-        let reward = done ? -10 : (g.obstacle.givePoints(g.bird)) * 10
+        let s=(g.obstacle.givePoints(g.bird))
+        let reward = done ? -1 : s
+        this.score+=s
 
         let nexState = this.getState(g)
         let m = {
@@ -102,18 +110,21 @@ export class Agent {
 
         this.shortMemory.push(m)
 
-        if (reward != 5 || this.shortMemory.length > 2 * fps) {
+        if (reward != 0) {
             this.shortMemory.map((v, i) => (v.reward = reward * ((this.shortMemory.length - i) ** (-1))))
+            // this.shorMemoryTrain(0.01)
             this.remember()
             this.shortMemory = []
         }
+        document.getElementById("score")!.innerText=this.score+""
 
         if (g.obstacle.collide(g.bird)) {
             g.restart()
-            this.longMemoryTrain(0.01)
+            this.longMemoryTrain(Number((document.getElementById("learning-rate") as HTMLInputElement).value )||0.01)
 
             this.totalGames++
             this.epsilon--
+            this.score=0
             document.getElementById("epoch")!.innerText = this.totalGames + ""
 
         }
